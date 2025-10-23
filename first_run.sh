640b65eed8937a90c97c80c9a00c08c9c7c6faf5
Tpj-root <trichy_hackerspace@outlook.com>
2025-10-23 18:12:44 +0530

    And Here We Go

---
+++ first_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# add aliases to bash.bashrc easily
+
+FILE="/data/data/com.termux/files/usr/etc/bash.bashrc"
+
+add_alias() {
+  local ALIAS_LINE="$1"
+  if ! grep -qxF "$ALIAS_LINE" "$FILE"; then
+    echo "$ALIAS_LINE" >> "$FILE"
+    echo "✅ Added: $ALIAS_LINE"
+  else
+    echo "⚡ Already exists: $ALIAS_LINE"
+  fi
+}
+
+# example alias
+add_alias "alias edit='nano /data/data/com.termux/files/usr/etc/bash.bashrc'"
+
+# later you can just add more aliases like:
+# add_alias "alias ll='ls -la'"
+# add_alias "alias gs='git status'"
+
